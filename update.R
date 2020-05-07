1f73eb699398a9c343dd6a8c9536c130916161c3
Jason St. George <stgeorgejas@gmail.com>
2020-05-07 09:35:05 -0400

    update dump

--- update.R
+++ update.R
@@ -2,35 +2,71 @@
 
 repos <- "https://cran.rstudio.com"
 
-lib.loc <- Sys.getenv("R_LIBS_USER")
-if(lib.loc == "")
-  lib.loc <- Sys.getenv("R_LIBS_SITE")
 
-lib.loc <- if (lib.loc == "")
-  .libPaths()[[1]] else
-  strsplit(lib.loc, ":", fixed = TRUE)[[1]][[1]]
+Sys.setenv("R_PROFILE_USER" = '')
+Sys.setenv("R_ENVIRON_USER" = '')
 
-Ncpus <- as.integer(
-  as.integer(system("grep ^processor /proc/cpuinfo | wc -l", TRUE)) * .8)
+Ncpus <- as.integer( .8 *
+  as.integer(system("grep ^processor /proc/cpuinfo | wc -l", TRUE)))
 
-ops <- options(warn = -1)
 
-message(paste(sep = "\n",
-  sprintf("Updating packages in:\n%s", paste("  ", .libPaths(), collapse = "\n")),
-  sprintf("Newer version will be installed in: \n %s", lib.loc),
-  sprintf("Using up to %i cores", Ncpus)))
+options(Ncpus = Ncpus,
+        repos = "http://cran.rstudio.org")
 
-withCallingHandlers(
-  update.packages(
-    instlib = lib.loc,
-    repos = repos,
-    checkBuilt = TRUE,
-    Ncpus = Ncpus,
-    ask = FALSE,
-    INSTALL_opts = "--use-vanilla"
-  ),
-  warning = function(w) {
-    if (grepl(.Library, w$message, fixed = TRUE))
-      invokeRestart("muffleWarning")
+
+
+get_writable_libs <- function() vapply(.libPaths(),  function(lib)
+  dir.exists(lib) && file.access(lib, mode = 2) == 0, TRUE)
+
+is_writable <- get_writable_libs()
+
+if(!any(is_writable)) {
+  user_lib <- Sys.getenv("R_LIBS_USER")
+  warning("*** Creating user library at ", user_lib, " ***")
+  user_lib <- normalizePath(user_lib, mustWork = FALSE)
+  if(!dir.exists(user_lib)) dir.create(user_lib, recursive = TRUE)
+  .libPaths(c(user_lib, .libPaths()))
+  is_writable <- get_writable_libs()
+}
+
+
+# --- update CRAN packages. ---
+#
+# for directories where we don't have write permissions, if the package is
+# masked then ignore it otherwise install it in the first entry on the search
+# path.
+
+x <- lapply(.libPaths(), function(lib) {
+  oldPkgs <- old.packages(lib, checkBuilt = TRUE)
+  if(is.null(oldPkgs)) return(NULL)
+  if (!is_writable[lib]) {
+    is_not_masked <-
+      vapply(rownames(oldPkgs),
+             function(pkg) find.package(pkg) == file.path(lib, pkg), TRUE,
+             USE.NAMES = FALSE)
+    if(!any(is_not_masked)) # all false
+      return(NULL)
+    oldPkgs <- oldPkgs[is_not_masked,, drop = FALSE]
+    oldPkgs[,"LibPath"] <- .libPaths()[1L]
   }
-)
+  oldPkgs[,c("Package", "LibPath")]
+})
+
+
+oldPkgs <- do.call(rbind, x)
+oldPkgs <- unique(oldPkgs)
+
+
+
+install.packages(oldPkgs[,"Package"], oldPkgs[,"LibPath"],
+                 dependencies = FALSE,
+                 INSTALL_opts = "--use-vanilla")
+
+
+# withCallingHandlers(
+#   do.call(update.packages, args),
+#   warning = function(w) {
+#     if (grepl(.Library, w$message, fixed = TRUE))
+#       invokeRestart("muffleWarning")
+#   }
+# )
